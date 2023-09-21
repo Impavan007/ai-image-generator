77e30693ba57a5c90cdaf3c076029a65708346dc
pavan <Impavan007>
2023-09-21 16:40:27 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
    <>
    <BrowserRouter>
    <header className="w-full flex  justify-between items-center bg-gray-900 sm:px-8 py-2 border-b border-b-[#e6ebf4]">
-    <Link to='/'><img src='../public/img1.png' alt="logo" className="w-28 object-contain mt-2"/></Link>
+    <Link to='/'><img src='../assets/logo.svg' alt="logo" className="w-28 object-contain mt-2"/></Link>
     <Link to ="create=post" className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md mt-2'>Create</Link>
     </header>
     <main className='sm:p-8 px-4 py-8 w-full bg-gray-200 min-h-[calc(100vh-73px)]'>
